a65fb5a026ee53d4b86479ce84c2ddce4e79a4b6
Suman Sharma <suman.191544@ncit.edu.np>
2023-06-01 20:11:53 +0545

    Adding Next & Previous Button

--- src/components/News.js
+++ src/components/News.js
@@ -3,26 +3,46 @@ import Newsitem from "./Newsitem";
 import "../css/main.css";
 
 export class News extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       articles: [],
       loading: false,
+      page: 1,
     };
   }
 
   async componentDidMount() {
     let url =
-      "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=fc8eaac95fb8492fb03297c640b838e5";
+      "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=fc8eaac95fb8492fb03297c640b838e5&pagesize=12";
     const data = await fetch(url);
     const parsedata = await data.json();
     // console.log(parsedata);
     this.setState({ articles: parsedata.articles });
   }
-  render() {
-    // console.log("hii i am render ");
 
+  handleNext = async () => {
+    console.log("next");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=fc8eaac95fb8492fb03297c640b838e5&pagesize=12&page=${
+      this.state.page + 1
+    }`;
+    const data = await fetch(url);
+    const parsedata = await data.json();
+    this.setState({ articles: parsedata.articles, page: this.state.page + 1 });
+  };
+
+  handlePrev = async () => {
+    console.log("Prev");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=fc8eaac95fb8492fb03297c640b838e5&pagesize=12&page=${
+      this.state.page - 1
+    }`;
+    const data = await fetch(url);
+    const parsedata = await data.json();
+    this.setState({ articles: parsedata.articles, page: this.state.page - 1 });
+  };
+
+  // -------------------------
+  render() {
     return (
       <>
         <div className="main">
@@ -41,6 +61,26 @@ export class News extends Component {
                 </div>
               );
             })}
+          </div>
+          <div className="d-flex justify-content-between">
+            <button
+              type="button"
+              disabled={this.state.page <= 1}
+              className="btn btn-info m-3"
+              onClick={this.handlePrev}
+            >
+              &laquo; Previous
+            </button>
+            <button type="button" className="btn btn-danger m-3">
+              &#8249;Happy Face, Happy Reader&#8250;
+            </button>
+            <button
+              type="button"
+              className="btn btn-warning m-3"
+              onClick={this.handleNext}
+            >
+              Next &raquo;
+            </button>
           </div>
         </div>
       </>
